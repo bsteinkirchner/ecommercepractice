42f549910240fb930b291e7d524a9f3bba79680e
Brittany Steinkirchner <bsteinkirchner@outlook.com>
2022-06-23 11:59:11 -0500

    break time

--- shopping-template/src/components/Navbar.jsx
+++ shopping-template/src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from "styled-components";
+import { Search, ShoppingCartOutlined } from '@material-ui/icons';
+import { Badge } from "@material-ui/core";
 
 const Container = styled.div`
     height: 60px;
@@ -8,24 +10,65 @@ const Wrapper = styled.div`
     padding: 10px 20px;
     display: flex;
     justify-content: space-between;
+    align-items: center;
 `;
 const Left= styled.div`
-
+    flex: 1;
+    display: flex;
+    align-items: center;
+`;
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+`;
+const SearchContainer = styled.div`
+    border: 0.5px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`;
+const Input = styled.input`
+    border: none;
 `;
 const Center= styled.div`
-
+    flex: 1;
+    text-align: center;
+`;
+const Logo = styled.h1`
+    font-weight: bold;
 `;
 const Right= styled.div`
-
+    flex: 1;
+`;
+const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
 `;
 
 function NavBar() {
   return (
     <Container>
         <Wrapper>
-            <Left>Left</Left>
-            <Center>Center</Center>
-            <Right>Right</Right>
+            <Left>
+                <Language>EN</Language>
+                <SearchContainer>
+                    <Input />
+                    <Search/>
+                </SearchContainer>
+            </Left>
+            <Center>
+                <Logo>SPEND.</Logo>
+            </Center>
+            <Right>
+                <MenuItem>REGISTER</MenuItem>
+                <MenuItem>SIGN IN</MenuItem>
+                <MenuItem>
+                    <Badge badgeContent ={4} color="primary">
+                        <ShoppingCartOutlined />
+                    </Badge>
+                </MenuItem>
+            </Right>
         </Wrapper>
     </Container>
   )
